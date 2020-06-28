d5bacf2410161d9e11ba974081cf05253afd03cb
guillermo96 <guillermo.williamson.96@gmail.com>
2020-06-28 18:48:33 -0300

    triggers listos para correrse en mysql

--- triggers.sql
+++ triggers.sql
@@ -20,80 +20,129 @@
 El caracter agua ‘A’ en el terreno es inmodificable, verificar eso
 */
 
+DELIMITER //
 CREATE TRIGGER NO_MODIFICAR_ARMAS
-BEFORE UPDATE OR DELETE ON ARMA
+BEFORE UPDATE ON arma
+FOR EACH ROW
 BEGIN
-	RAISE_APPLICATION_ERROR(-20001,'No se puede modificar el inventario de armas') ;
-END;
+	
+	signal SQLSTATE '45001' SET message_text = 'No se puede modificar el inventario de armas!';     
+END; //
+DELIMITER ;
 
-CREATE TRIGGER NO_MODIFICAR_CONFIGURACION
-BEFORE UPDATE OR DELETE ON CONFIGURACION
+DELIMITER //
+CREATE TRIGGER NO_ELIMINAR_ARMAS
+BEFORE DELETE ON arma
+FOR EACH ROW
 BEGIN
-	RAISE_APPLICATION_ERROR(-20001,'No se puede modificar las opciones de configuración') ;
-END;
+	
+	signal SQLSTATE '45002' SET message_text = 'No se puede modificar el inventario de armas!';     
+END; //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER NO_MODIFICAR_CONFIGURACION
+BEFORE UPDATE ON configuracion
+FOR EACH ROW
+BEGIN
+	
+	signal SQLSTATE '45001' SET message_text = 'No se puede modificar las opciones de configuración!';     
+END; //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER NO_ELIMINAR_CONFIGURACION
+BEFORE DELETE ON configuracion
+FOR EACH ROW
+BEGIN
+	
+	signal SQLSTATE '45002' SET message_text = 'No se puede eliminar las opciones de configuración!';     
+END; //
+DELIMITER ;
 
 
 --2
 
+DELIMITER //
 CREATE TRIGGER PartidaEnCurso_Terreno
-BEFORE UPDATE ON Terreno
+BEFORE UPDATE ON terreno
+FOR EACH ROW
 BEGIN
 	DECLARE partidaID INT;
-	SET partidaID = SELECT Partida_ID FROM Partida p , Terreno_Tipo tt
-					WHERE p.Terreno_ID = tt.Terreno_ID 
-					AND tt.Terreno_ID = new.Terreno_ID;
-
+	SET partidaID = (SELECT p.Partida_ID FROM partida p , terreno t
+					WHERE p.Terreno_ID = t.Terreno_ID 
+					AND t.Terreno_ID = OLD.´Terreno_ID´);
+ 
 	IF (NOT PartidaEnCurso(partidaID))
-			RAISE_APPLICATION_ERROR(-20001,'La partida no esta en curso') ;
-END;
+			THEN signal SQLSTATE '45003' SET message_text = 'La partida no esta en curso'; 
+	END IF;
+END; //
+DELIMITER ;
 
 
-
+DELIMITER //
 CREATE TRIGGER PartidaEnCurso_Equipo
-BEFORE UPDATE ON Equipo
+BEFORE UPDATE ON equipo
+FOR EACH ROW
 BEGIN
 	DECLARE partidaID INT;
-	SET partidaID = GetPartidaID(new.Equipo_ID);
+	SET partidaID = GetPartidaID(new.´Equipo_ID´);
 
 	IF (NOT PartidaEnCurso(partidaID))
-			RAISE_APPLICATION_ERROR(-20001,'La partida no esta en curso') ;
-END;
+			THEN signal SQLSTATE '45004' SET message_text = 'La partida no esta en curso'; 
+	END IF;
+END; //
+DELIMITER ;
 
 
+DELIMITER //
 CREATE TRIGGER PartidaEnCurso_Gusano
-BEFORE UPDATE ON Gusano
+BEFORE UPDATE ON gusano
+FOR EACH ROW
 BEGIN
 	DECLARE partidaID INT;
-	SET partidaID = GetPartidaID((SELECT Equipo_ID FROM Equipo_Gusanos WHERE Gusano_ID = new.Gusano_ID));
+	SET partidaID = GetPartidaID((SELECT Equipo_ID FROM Equipo_Gusanos WHERE Gusano_ID = new.´Gusano_ID´));
 
 	IF (NOT PartidaEnCurso(partidaID))
-			RAISE_APPLICATION_ERROR(-20001,'La partida no esta en curso') ;
-END;
+			THEN signal SQLSTATE '45005' SET message_text = 'La partida no esta en curso';
+	END IF;
+END; //
+DELIMITER ;
 
 --Atributo control en tabla equipo
 
+DELIMITER //
 CREATE TRIGGER AttrControl_TablaEquipo
 BEFORE UPDATE ON Equipo
+FOR EACH ROW
 BEGIN
-	IF (new.Control != old.Control)
-		RAISE_APPLICATION_ERROR(-20001,'No se puede cambiar quien controla el equipo');
-END;
+	IF (new.´Control´ != old.´Control´)
+		THEN signal SQLSTATE '45006' SET message_text = 'No se puede cambiar quien controla el equipo';
+	END IF;
+END; //
+DELIMITER ;
 
 --Update en qeuipoGusano = error
 
 
 
+DELIMITER //
 CREATE TRIGGER EquipoGusano_NoUpdate
-BEFORE UPDATE ON Equipo_Gusanos
+BEFORE UPDATE ON equipo_gusanos
+FOR EACH ROW
 BEGIN
-		RAISE_APPLICATION_ERROR(-20001,'No se puede hacer update en la tabla equipo gusano');
-END;
+	signal SQLSTATE '45007' SET message_text = 'No se puede hacer update en la tabla equipo gusano';
+END; //
+DELIMITER ;
 
 
-
+DELIMITER //
 CREATE TRIGGER NoQuitarAgua_Terreno
-BEFORE UPDATE ON Terreno
+BEFORE UPDATE ON terreno
+FOR EACH ROW
 BEGIN
-		IF (old.Celda = 'A' AND new.Celda != 'A')
-			RAISE_APPLICATION_ERROR(-20001,'No puede quitarse el agua del terreno');
-END;	
+		IF (old.´Celda´ = 'A' AND new.´Celda´ != 'A')
+			THEN signal SQLSTATE '45008' SET message_text = 'No puede quitarse el agua del terreno';
+		END IF;
+END; //
+DELIMITER ;	
